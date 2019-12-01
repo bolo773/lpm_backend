80dae10254d8d7165cf7e0e543df2ed752c856b4
bolo <bolojesus@gmail.com>
2019-12-01 10:50:34 -0600

    new cleaning

--- src/image_dec.php
+++
@@ -1,8 +0,0 @@
-<?php
-$key="01234567890123456789012345678901";
-$iv="0123456789012345";
-$data = file_get_contents('php://stdin');
-$output= openssl_decrypt($data,"AES-256-CBC", $key, OPENSSL_RAW_DATA, $iv);
-
-file_put_contents('phpdec.png',$output);
-?>
